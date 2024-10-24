187b36a16366965101bc37f5dc7d19d6d7bd2d15
Marcin Siennicki <m.siennicki@cloos.pl>
2024-10-24 14:55:30 +0200

    nvchad 3.0, lspconfig ts_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,6 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+
 local servers = { "html", "cssls" }
 
 -- local util = require "lspconfig/util"
@@ -17,8 +18,8 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
+-- -- typescript
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
