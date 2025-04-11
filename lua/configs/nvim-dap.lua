811553cfba356e01ffd1a6cbd9ab4eb8e630c258
Marcin Siennicki <m.siennicki@cloos.pl>
2025-04-11 15:31:39 +0200

    konfiguracja dap wyniesiona do oddzielnego pliku, lldb-dap znowu nie dziala

--- lua/configs/nvim-dap.lua
+++ lua/configs/nvim-dap.lua
@@ -4,6 +4,82 @@ local map = vim.keymap.set
 local dap = require("dap")
 local dapui = require("dapui")
 
+local dap = require('dap')
+
+-- Konfigruracja adaptera codelldb
+local codelldb = false
+local codelldb_command = "codelldb"
+local mason_registry = require "mason-registry"
+if mason_registry.is_installed("codelldb") then
+  local codelldb = mason_registry.get_package("codelldb")
+  local codelldb_path = codelldb:get_install_path() .. "/extension/adapter/"
+  codelldb_command = codelldb_path .. codelldb_command
+  codelldb = true
+elseif vim.fn.executable(codelldb_command) then
+  codelldb = true
+end
+
+if codelldb then
+  dap.adapters.codelldb = {
+    type = "executable",
+    command = codelldb_command,
+    detached = false,
+  }
+end
+
+-- Konfigruracja adaptera lldb-dap
+local lldbdap = false
+local lldbdap_command = "lldb-dap"
+if vim.fn.executable(lldbdap_command) then
+  lldbdap_command = vim.fn.exepath(lldbdap_command)
+  lldbdap = true
+end
+
+if lldbdap then
+  dap.adapters.lldb = {
+    type = "executable",
+    command = lldbdap_command,
+    name = "lldb",
+  }
+end
+
+-- Konfiguracja jezykow c/c++/rust
+if codelldb then
+  dap.configurations.cpp = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+    },
+  }
+  dap.configurations.c = dap.configurations.cpp
+  dap.configurations.rust = dap.configurations.cpp
+
+elseif lldbdap then
+  dap.configurations.cpp = {
+    {
+      name = "Launch",
+      type = "lldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
+      initCommands = "",
+    },
+  }
+  dap.configurations.c = dap.configurations.cpp
+  dap.configurations.rust = dap.configurations.cpp
+end
+
+-- Skroty klawiszowe do dapui
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open();
 end
--- lua/configs/rustaceanvim.lua
+++ lua/configs/rustaceanvim.lua
@@ -1,47 +1,35 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+local codelldb_command = "codelldb"
+local codelldb_port = "${port}"
 
 local mason_registry = require "mason-registry"
-
-if not mason_registry.is_installed("codelldb") then
-  vim.cmd("MasonInstall codelldb")
+if mason_registry.is_installed("codelldb") then
+  local codelldb = mason_registry.get_package("codelldb")
+  local codelldb_path = codelldb:get_install_path() .. "/extension/adapter/"
+  codelldb_command = codelldb_path .. codelldb_command
 end
-
-local codelldb = mason_registry.get_package("codelldb")
-local extension_path = codelldb:get_install_path() .. "/extension/"
-local codelldb_command = extension_path .. "adapter/codelldb"
--- local liblldb_path = extension_path .. "lldb/lib/liblldb.lib"
-local codelldb_port = "${port}"
 
 local map = vim.keymap.set
 
--- local lldb_command = "lldb"
 vim.g.rustaceanvim = {
-  dap = {
-    -- adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb_path, liblldb_path)
-    adapter = {
-      type = "server",
-      port = codelldb_port,
-      host = "127.0.0.1",
-      executable = {
-        command = codelldb_command,
-        args = {
-          "--port",
-          codelldb_port,
-        },
-      },
-    },
-  },
+  -- dap = {
+  --   adapter = {
+  --     type = "server",
+  --     port = codelldb_port,
+  --     host = "127.0.0.1",
+  --     executable = {
+  --       command = codelldb_command,
+  --       args = {
+  --         "--port",
+  --         codelldb_port,
+  --       },
+  --     },
+  --   },
+  -- },
   server = {
     on_attach = function(_, _)
-      on_attach()
       map("n", "<leader>k", ":RustLsp hover actions<CR>")
       map("n", "<leader>a", ":RustLsp codeAction<CR>")
-      -- map("n", "<leader>rn", vim.lsp.buf.rename)
     end,
-    capabilities = capabilities,
-    on_init = on_init,
   },
   tools = {
     float_win_config = {
